54316ce32640e538e902949632be35cb5dacbc4f
Alex <alex.garcia.arias@gmail.com>
2020-04-18 13:59:38 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FotosComponent } from './components/fotos/fotos.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { AppRoutingModule } from './app.routes';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+
+import { environment } from '../environments/environment';
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FotosComponent,
+    CargaComponent,
+    NgDropFilesDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule
+  ],
+  providers: [
+    CargaImagenesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
